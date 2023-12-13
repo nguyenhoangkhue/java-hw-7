07546a5fee51b6192832e90dae82564f63ef013c
nguyenhoangkhue <snh110685@gmail.com>
2023-12-13 15:40:03 +0700

    upd

---
+++ HW7/src/Main/Book.java
@@ -0,0 +1,63 @@
+package Main;
+
+public class Book {
+    private String id;
+    private String title;
+    private String author;
+    private String category;
+    private String publisher;
+    private int year;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    @Override
+    public String toString(){
+        return  id+"-"+title+"-"+author+"-"+category+"-"+publisher+"-"+year;
+    }
+}
---
+++ HW7/src/Main/Hw.java
@@ -0,0 +1,70 @@
+package Main;
+import java.util.Scanner;
+
+public class Hw {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+
+        Book book=new Book();
+        book.setId("1234");
+        book.setTitle("Huhu");
+        book.setAuthor("Khue");
+        book.setCategory("action");
+        book.setPublisher("Khue");
+        book.setYear(2023);
+
+        while (true){
+            System.out.println("Book management");
+            System.out.println("Enter 1: To search books by title");
+            System.out.println("Enter 2: To search books by category");
+            System.out.println("Enter 3: To list books published this year");
+            String line= sc.nextLine();
+            switch (line){
+                case "1":{
+                    System.out.println("Enter title: ");
+                    String title=sc.nextLine();
+                    if (title==Title(book)){
+                        System.out.println(showInfo(book));
+                        break;
+                    } else {
+                        System.out.println("Invalid");
+                    }
+                    break;
+                }
+                case "2":{
+                    System.out.println("Enter category: ");
+                    String category=sc.nextLine();
+                    if (category==Category(book)){
+                        System.out.println(showInfo(book));
+                        break;
+                    }else {
+                        System.out.println("Invalid");
+                    }
+                    break;
+                }
+                case "3":{
+                    if (book.getYear()==2023){
+                        System.out.println(showInfo(book));
+                        break;
+                    }else {
+                        System.out.print("");
+                    }
+                    break;
+                }
+                default:{
+                    System.out.println("Invalid");
+                    break;
+                }
+            }
+        }
+    }
+    public static String showInfo(Book book){
+        return book.toString();
+    }
+    public static String Title(Book book){
+        return book.getTitle();
+    }
+    public static String Category(Book book){
+        return book.getCategory();
+    }
+}
